0cfc6b6fed5431bbd2996790d9858a30434183f5
Michał Simka <michal.simka@ostrzy.net>
2015-05-27 10:05:11 +0200

    Add Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527065840) do
+ActiveRecord::Schema.define(version: 20150527065853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "operations", force: :cascade do |t|
     t.string   "invoice_num",                             null: false
