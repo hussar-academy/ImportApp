8e56046ef23785c0cc4a3c6b4fa6701b34782396
Ahmad A.Elghany <tolpa1@gmail.com>
2016-05-07 14:09:57 +0200

    Fix :through typo

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,9 +1,12 @@
 class Operation < ActiveRecord::Base
   belongs_to :company
-  has_many :categorizations
-  has_many :operations, thorugh: :categorizations
 
-  validates_presence_of :invoice_num, :invoice_date, :amount, :operation_date, :kind, :status
+  has_many :categorizations
+  has_many :categories, through: :categorizations
+
+  validates_presence_of :invoice_num, :invoice_date, :amount,
+                        :operation_date, :kind, :status
+
   validates_numericality_of :amount, greater_than: 0
   validates_uniqueness_of :invoice_num
 end
