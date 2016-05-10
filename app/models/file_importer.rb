9836c0f812c4c64f492f26e7cc43e727e70608ee
Ahmad A.Elghany <tolpa1@gmail.com>
2016-05-10 22:47:57 +0200

    Extend the importer class

--- app/models/file_importer.rb
+++ app/models/file_importer.rb
@@ -2,27 +2,51 @@ require 'csv'
 
 # CSV file importer class
 class FileImporter
-  attr_reader :companies, :categories
+  attr_reader :companies, :categories, :non_parsed_rows, :rows
 
   def initialize(file)
+    @non_parsed_rows = 0
     scan(file)
   end
 
-  def scan(file)
-    categories = []
-    companies  = []
+  def scanner(file)
+    kinds       = []
+    companies   = []
+    rows        = []
 
     CSV.foreach(file, headers: true) do |row|
-      companies  << row['company']
-      categories << row['kind']
+      kinds << row['kind']
+      # Counting the non parsed rows due to absense of company field.
+      if row['company'].nil?
+        @non_parsed_rows += 1
+      else
+        companies << row['company']
+        rows.push({
+          company: row['company'],
+          invoice_num: row['invoice_num'],
+          operation_date: row['operation_date'],
+          amount: row['amount'],
+          reporter: row['reporter'],
+          notes: row['notes'],
+          kind: row['kind']
+        })
+      end
     end
-    @categories = categorize(categories)
+    @categories = kind_parser(kinds)
+    @companies  = company_parser(companies)
+    @rows       = rows_cleaner(rows)
   end
+  alias scan scanner
 
   private
 
-  def categorize(data)
-    parsed = data.join(' ').tr(';', ' ').gsub(/  /, ' ')
-    parsed.split(' ').map(&:capitalize).uniq!
+  def kind_parser(data)
+    data.join(' ').tr(';', ' ').gsub(/  /, ' ')
+        .split(' ').map(&:capitalize).uniq!
+  end
+
+  # Clean the spaces before company name if there any.
+  def company_parser(data)
+    data.map(&:lstrip)
   end
 end
--- spec/models/file_importer_spec.rb
+++ spec/models/file_importer_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe FileImporter, type: :model do
-  let(:companies) { Companies.order('id asc') }
-  file = File.path("spec/fixtures/ImporterApp.csv")
+  let(:categories) { ["Client", "Delegation", "Important", "Internal", "Negligible", "Other"] }
+  file = File.path('spec/fixtures/ImporterApp.csv')
 
-  it "can read the csv file" do
+  it "should read the csv file" do
     importer = FileImporter.new(file)
     expect(importer).to be_present
   end
 
+  it "should scan the file" do
+    importer = FileImporter.new(file)
+    expect(importer.categories).to match_array(categories)
+  end
 end
