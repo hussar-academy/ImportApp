afed43c1f1a5bd63be70cc317874abc20002a56b
mikgry <mikolaj0grygiel@gmail.com>
2020-07-07 21:11:48 +0200

    Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # We don't want to overcomplicate, so let this be just disabled
+  # We n't want to overcomplicate, so let this be just disabled
   # protect_from_forgery with: :exception
 end
